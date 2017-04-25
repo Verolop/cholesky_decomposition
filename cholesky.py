3400da89445616a79f1dd04520be325846413b84
Verolop <veronika@ciencias.unam.mx>
2017-04-25 15:01:16 -0500

    first commit

---
+++ cholesky.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from math import sqrt
+from pprint import pprint
+
+def cholesky(A):    
+    n = len(A)
+    
+    L = [[0.0] * n for i in xrange(n)]
+    
+    for i in xrange(n):
+        for k in xrange(i+1):
+            suma = sum(L[i][j]* L[k][j] for j in xrange(k))
+
+            if (i==k):
+                L[i][k] = sqrt(A[i][i] - suma)
+            else:
+                L[i][k] = (1.0 / L[k][k]* (A[i][k] - suma))
+
+    return L 
+
+A= [[1, 3, 5], [3, 13, 23], [5, 23, 42]]
+L = cholesky(A)
+
+print "Matriz A= "
+pprint(A)
+
+print "L="
+pprint(L)
+
